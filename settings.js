44b643789dac8ec04d633019e616de31209dfb71
Brett Jankord <bjankord@gmail.com>
2019-04-15 15:43:01 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,38 +1,57 @@
 {
-    "atomKeymap.promptV3Features": true, // Use atom keymap
-    "editor.fontSize": 12, // Font size
-    "editor.emptySelectionClipboard": false, // Prevents issue when I copy empty selection, it will keep my prior selection
-    "editor.minimap.enabled": false, // Hide the minimap
+    "atomKeymap.promptV3Features": true,
+    "editor.fontSize": 12,
+    "editor.emptySelectionClipboard": false,
+    "editor.minimap.enabled": false,
+    "editor.rulers": [100],
     "editor.tabSize": 2,
-    "editor.scrollBeyondLastLine": false, // Don't overscroll
+    "editor.scrollBeyondLastLine": false,
     "editor.selectionHighlight": false,
-    "editor.wordSeparators": "`~!@#%^&*()=+[{]}\\|;:'\",.<>/?", // Removed $ and - from default word separartors. Makes it easier to copy variables
-    "editor.multiCursorModifier": "ctrlCmd",
-    "editor.formatOnPaste": true,
+    "editor.wordSeparators": "`~!@#%^&*()=+[{]}\\|;:'\",.<>/?",
     "files.autoSave": "afterDelay",
-    "files.defaultLanguage": "markdown", // When opening a new blank file, it will default to markdown syntax
-    "files.trimTrailingWhitespace": true, // Trim dem spaces
+    "files.defaultLanguage": "markdown",
+    "files.trimTrailingWhitespace": true,
     "files.trimFinalNewlines": true,
     "files.insertFinalNewline": false,
-    "explorer.openEditors.visible": 0, // Hides the open editors
-    "search.location": "panel", // Improves search display so it displays in popup pannel
-    "window.zoomLevel": 1, // Hack to increase fontsize of sidebar, this pairs with the editor.fontSize being set at 12
-    "workbench.iconTheme": "eq-material-theme-icons-ocean", // Nice icons
-    "workbench.colorTheme": "One Dark Pro", // Theme similar to Atom One Dark
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.formatOnPaste": true,
+    "explorer.openEditors.visible": 0,
+    "search.location": "panel",
+    "window.zoomLevel": 1,
+    "workbench.colorTheme": "One Dark Pro",
     "workbench.colorCustomizations": {
-      "sideBar.background": "#262831", // Matches Atom One Dark
-      "sideBar.foreground": "#9ea1b3", // Matches Atom One Dark
-      "gitDecoration.ignoredResourceForeground": "#6e717f", // Matches Atom One Dark
-      "editor.background": "#31343f", // Matches Atom One Dark
-      "list.activeSelectionBackground": "#4f65d1", // Matches Atom One Dark
-      "list.inactiveSelectionBackground": "#3f4250" // Matches Atom One Dark
+      "sideBar.background": "#262831",
+      "sideBar.foreground": "#9ea1b3",
+      "gitDecoration.ignoredResourceForeground": "#6e717f",
+      "editor.background": "#31343f",
+      "list.activeSelectionBackground": "#4f65d1",
+      "list.inactiveSelectionBackground": "#3f4250"
     },
-    "workbench.activityBar.visible": false, // Hide the activitybar
-    "workbench.startupEditor": "none", // Hide the welcome page on startup
+    "workbench.startupEditor": "none",
     "gitProjectManager.baseProjectsFolders": [
-      "/Users/bj031910/repos" // where I keep my git repos
+      "/Users/bj031910/repos"
     ],
     "gitProjectManager.ignoredFolders": [
-      "node_modules" 
+      "node_modules"
     ],
-}
+    "stylelint.configOverrides": {
+      "ignoreFiles": "**/*.jsx"
+    },
+    "explorer.confirmDelete": false,
+    "explorer.confirmDragAndDrop": false,
+    "javascript.updateImportsOnFileMove.enabled": "always",
+    "editor.accessibilitySupport": "off",
+    "cSpell.userWords": [
+      "Cerner",
+      "activedescendant",
+      "combobox",
+      "dropdown",
+      "listbox",
+      "polyfill",
+      "prepend",
+      "proptypes",
+      "spacebar",
+      "viewports"
+    ],
+    "workbench.iconTheme": "eq-material-theme-icons-ocean"
+  }
